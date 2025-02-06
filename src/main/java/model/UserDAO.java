20d397c44e0fdfb93e0428859ec5ea87c47bf0c3
PleaseErwin <rosec77@sookmyung.ac.kr>
2025-02-06 19:00:56 +0900

    [feat] Update DAO DTO

--- src/main/java/model/UserDAO.java
+++ src/main/java/model/UserDAO.java
@@ -1,5 +1,37 @@
 package model;
 
-public class UserDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import model.domain.UserDTO;
+import util.DBUtil;
+
+public class UserDAO {
+	
+	public static boolean getUser(String id, String pw) throws Exception {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		//UserDTO user = null;
+		boolean login = false;
+		
+		try {
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement("select * from user where id = ? AND pw = ?");
+			pstmt.setString(1, id);
+			pstmt.setString(2, pw);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				login = true;
+			}
+			
+		} finally {
+			DBUtil.close(con, pstmt, rs);
+		}
+		
+		return login;
+	}
 }
--- src/main/java/model/domain/UserDTO.java
+++ src/main/java/model/domain/UserDTO.java
@@ -1,5 +1,11 @@
 package model.domain;
 
 public class UserDTO {
-
+	String id;
+    String pw;
+    enum grade {
+    	admin, user
+    }
+    String pw_question;
+    String pw_answer;
 }
