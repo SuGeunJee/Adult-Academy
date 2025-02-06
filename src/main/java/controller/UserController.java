1c9f43c1ff206e22dfdef4abf0a0edb16f286d6f
PleaseErwin <rosec77@sookmyung.ac.kr>
2025-02-06 20:51:38 +0900

    [feat] Update DAO DTO Controller

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,5 +1,56 @@
 package controller;
 
-public class UserController {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.UserDAO;
+
+@WebServlet("/login")
+public class UserController extends HttpServlet {
+	
+protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String command = request.getParameter("command");// 로그인인지 회원가입인지 비밀번호찾기인지 구분
+		
+		if (command == null) {
+			response.sendRedirect("login.html");
+		} 
+		
+		else if (command.equals("login")) {// 로그인
+			try {
+				String id = request.getParameter("id");
+				String pw = request.getParameter("pw");
+				
+				System.out.println(id);
+				boolean check = UserDAO.getUser(id, pw);
+				System.out.println(check);
+				
+				if (check == true) request.getRequestDispatcher("main.jsp").forward(request, response);// f
+				else {
+					response.setContentType("text/html;charset=UTF-8");
+					PrintWriter out = response.getWriter();
+					out.println("<script>alert('로그인 오류'); location.href='login';</script>");// id랑 pw 유지하지 않으므로 r
+					out.close();
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} 
+		
+		else if (command.equals("signup")) {// 회원가입
+			response.sendRedirect("signup.jsp");// r
+		}
+		
+		else if (command.equals("searchpw")) {// 비밀번호 찾기
+			response.sendRedirect("searchpw.jsp");// r
+		}
+		
+	}
 
 }
--- src/main/java/model/UserDAO.java
+++ src/main/java/model/UserDAO.java
@@ -4,34 +4,78 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-import model.domain.UserDTO;
 import util.DBUtil;
 
 public class UserDAO {
 	
-	public static boolean getUser(String id, String pw) throws Exception {
+	public static boolean getUser(String id, String pw) throws Exception {// 로그인
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		
-		//UserDTO user = null;
-		boolean login = false;
+		boolean userId = false;
 		
 		try {
 			con = DBUtil.getConnection();
-			pstmt = con.prepareStatement("select * from user where id = ? AND pw = ?");
+			pstmt = con.prepareStatement("SELECT id FROM user WHERE id = ? AND pw = ?");
 			pstmt.setString(1, id);
 			pstmt.setString(2, pw);
 			rs = pstmt.executeQuery();
 			
 			if (rs.next()) {
-				login = true;
+				userId = true;
 			}
 			
 		} finally {
 			DBUtil.close(con, pstmt, rs);
 		}
 		
-		return login;
+		return userId;
+	}
+	
+	public static boolean addUser(String id, String pw, String grade, String pw_question, String pw_answer) throws Exception {// 회원가입
+		Connection con = null;	
+		PreparedStatement pstmt = null;
+		
+		try {
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement("INSERT INTO user VALUES (?, ?, ?, ?, ?)");
+	       
+			pstmt.setString(1, id);
+	        pstmt.setString(2, pw);
+	        pstmt.setString(3, grade);
+	        pstmt.setString(4, pw_question);
+	        pstmt.setString(5, pw_answer);
+	        
+			if(pstmt.executeUpdate() != 0) {
+				return true;
+			}
+			
+		} finally {
+			DBUtil.close(con, pstmt);
+		}
+		
+		return false;
+	}
+	
+	public static boolean confirmUser(String id) throws Exception {// 비밀번호찾기시 id 존재 여부 확인
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement("select id from user where id = ?");
+			
+			pstmt.setString(1, id);
+			
+			if(pstmt.executeUpdate() != 0) {
+				return true;
+			}
+			
+		} finally {
+			DBUtil.close(con, pstmt);
+		}
+		
+		return false;
 	}
 }
